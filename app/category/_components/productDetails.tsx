52a4718fb1b97c20e6cd6f319d756887f2993305
shaheeeen <shaheeenmuhammed@gmail.com>
2025-09-26 20:32:23 +0400

    fixes

--- app/category/_components/productDetails.tsx
+++ app/category/_components/productDetails.tsx
@@ -767,7 +767,7 @@ export default function ProductDetailPage({
                                 Returns
                               </h4>
                               <div className="space-y-1 leading-relaxed">
-                                <p>• No return or</p>
+                                <p>• No return or refund</p>
                               </div>
                             </div>
                           </div>
